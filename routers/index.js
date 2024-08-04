8a96f633cc52de6ea6540c27e6b3a928df3f0545
0wenB <bryanowen.code@gmail.com>
2024-08-04 17:46:42 +0700

    update files,routes

--- routers/index.js
+++ routers/index.js
@@ -4,11 +4,11 @@ const router = require("express").Router();
 
 router.post("/login", Controller.login);
 router.post("/register", Controller.register);
+router.get("/", Controller.findAllNews);
+router.get("/berita/:newsId", Controller.findNewsById);
 
 router.use(authentication);
 
-router.get("/", Controller.findAllNews);
-router.get("/berita/:newsId", Controller.findNewsById);
 router.get("/profile", Controller.findUserInfo);
 
 module.exports = router;
